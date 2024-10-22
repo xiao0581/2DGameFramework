56417e69000bae685f848f2702a31759530f5824
xiaohui <xiaohui@XIAO-PC>
2024-10-22 20:13:32 +0200

    v3

--- SimpleGame/Program.cs
+++ SimpleGame/Program.cs
@@ -8,30 +8,29 @@ using Mandatory2DGameFramework.model.defence;
 using SimpleGame;
 
 IWorldObjectFactory factory = new WorldObjectFactory();
-World world = new World(10, 10, factory); // 创建一个10x10的世界
+World world = new World(10, 10, factory); 
 
-// 创建生物
-Creature warrior = new Warrior("Conan", 100, 2, 3); // 战士
-Creature mage = new Mage("Gandalf", 80, 1, 1); // 法师
 
-// 将生物添加到世界
+Creature warrior = new Warrior("Conan", 100, 2, 3); 
+Creature mage = new Mage("Gandalf", 80, 1, 1); 
+
+
 world.AddCreature(warrior);
 world.AddCreature(mage);
 
-// 创建物品：刀（攻击）、盾（防御）、陷阱、奖励
-world.AddWorldObject("Attack", "Sword", 15, 5, 5); // 刀
-world.AddWorldObject("Defence", "Shield", 10, 3, 3); // 盾
-world.AddWorldObject("Hazard", "Spike Trap", 20, 4, 4); // 陷阱
-world.AddWorldObject("Bonus", "Health Potion", 30, 2, 2); // 奖励
 
-// 生物移动并拾取物品
-warrior.Move(5, 5, world); // 战士移动到刀的位置并拾取
-mage.Move(2, 2, world); // 法师移动到奖励的位置并拾取
+world.AddWorldObject("Attack", "Sword", 15, 5, 5); 
+world.AddWorldObject("Defence", "Shield", 10, 3, 3); 
+world.AddWorldObject("Hazard", "Spike Trap", 20, 4, 4);
+world.AddWorldObject("Bonus", "Health Potion", 30, 2, 2); 
 
-// 生物攻击交互
-warrior.Hit(mage);  // 战士攻击法师
-mage.Hit(warrior);  // 法师反击战士
 
-// 战士走到陷阱，触发陷阱
-warrior.Move(4, 4, world); --------------------------------------------------------------------------------------------------------------------------------------------
+warrior.Move(5, 5, world); 
+mage.Move(2, 2, world); 
 
+
+warrior.Hit(mage);  
+mage.Hit(warrior); 
+
+
+warrior.Move(4, 4, world);
